c8d90ff25904d70fb8ab678f170258ef384dc242
Bemer <bemer@yandex-team.ru>
2014-12-10 15:30:24 +0400

    Add server/constants.js module

---
+++ server/constants.js
@@ -0,0 +1,17 @@
+module.exports = {
+    HANDLERS: {
+        INDEX: 'index',
+        GET_ISSUES: 'getIssues',
+        GET_ISSUE: 'getIssue',
+        CREATE_ISSUE: 'createIssue',
+        EDIT_ISSUE: 'editIssue',
+        GET_COMMENTS: 'getComments',
+        CREATE_COMMENT: 'createComment',
+        EDIT_COMMENT: 'editComment',
+        DELETE_COMMENT: 'deleteComment',
+        GET_LABELS: 'getLabels',
+        GET_AUTH_USER: 'getAuthUser',
+        AUTH: 'auth',
+        GET_REPO_INFO: 'getRepoInfo'
+    }
+};
